76c13570d831f9396dd5f32750dff8d460074233
Matt Kleven <klevennb115@gmail.com>
2019-03-04 00:55:57 -0600

    added logic

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        boolean testBool = false;
+        int myUnderstanding = 2;
+        int tomorrowsUnderstanding = myUnderstanding *2;
+//        System.out.println("Hello World!");
+        System.out.println(tomorrowsUnderstanding);
+
+        if (testBool != false) {
+
+            System.out.println("Good Gravy!");
+
+        } else if (tomorrowsUnderstanding > myUnderstanding) {
+
+            System.out.println("Progress!"); }
     }
 }
